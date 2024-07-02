332e819e872ae2457538a44d075adbcb267ddf4c
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-07-02 16:19:12 +0300

    UI almost done

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -3,13 +3,15 @@ import React, { useState } from "react";
 function Score(){
     const [wins, setWins] = useState({X: 0, O: 0, D: 0})
     return(
-        <div className="turn-container">
-            <h3>Total</h3>
-            <div className="turn-box align">X</div>
-            <div className="turn-box align">X</div>
-            <div className="turn-box align">X</div>
+        <>
+        <h3>Wins</h3>
+        <div className="total-container">
+            <div className="turn-box align">X: {wins.X}</div>
+            <div className="turn-box align">O: {wins.O}</div>
+            <div className="turn-box align">Draw: {wins.D}</div>
             
         </div>
+        </>
     )
 }
 
